d91052bee0df1f85fb550a4dedb325a102bb4e6c
Amith-Gururaj <Amithraj434@gmail.com>
2022-01-10 12:12:49 +0530

    Amith Java MSA 5

---
+++ src/test/java/amith/java/msa5/JavaMsa5ApplicationTests.java
@@ -0,0 +1,13 @@
+package amith.java.msa5;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaMsa5ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
